ebcdea3c128dcc332c5fdede9190e06cb50df2e5
Meicen <meicen.tw@gmail.com>
2019-03-11 13:31:15 +0800

    button2 ok

--- test190311_Hello/test190311_Hello/Form1.cs
+++ test190311_Hello/test190311_Hello/Form1.cs
@@ -16,5 +16,10 @@ namespace test190311_Hello
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
